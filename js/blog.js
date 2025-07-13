60941f4d8557c60fc4944242717dd42492014447
HaroldoWTS <conde_nostaw@hotmail.com>
2025-07-12 22:56:55 -0300

    frases

---
+++ js/blog.js
@@ -0,0 +1,16 @@
+function tapronto(){
+    $.getJSON('/js/frases.json', function(data) {
+	if (data.length > 0) {
+	    var randomIndex = Math.floor(Math.random() * data.length);
+	    var fraseAleatoria = data[randomIndex];
+	    $('blockquote.citacoes').text(fraseAleatoria);
+	    console.log("citacao: "+fraseAleatoria);
+	} else {
+	    console.log("citacao: nenhuma frase!")
+	}
+    }).fail(function() {
+	console.log('citacao: erro!')
+    });
+}
+
+$(document).ready(tapronto);
